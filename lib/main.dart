7c3980c3fbb90671713b68de55a55aa378d111cc
Nik <nik@Techstacks-Laptop.local>
2023-10-17 20:14:04 +0300

    add yml file for aws deploy v12

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'tech app'),
+      home: const MyHomePage(title: 'niks test deploy'),
     );
   }
 }
